3aea0e3c34a6da1b25009e2395cf4c31ccd24b88
kaka18mika <kaka18mika@gmail.com>
2024-05-27 13:03:01 -0300

    desafio criado

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("oi")
+let nome = "James"
+let experiencia = 2000
+let rank
+
+
+while (true) {
+    if (experiencia <=  1000) {
+        rank = "Ferro";
+    }
+    else if (experiencia > 1000 && experiencia <= 2000 ) {
+        rank = "Bronse";
+    }
+    else if (experiencia > 2000 && experiencia <=  5000) {
+        rank = "Prata";
+    }
+    else if (experiencia > 5000 && experiencia <= 7000) {
+        rank = "Ouro";
+    }
+    else if (experiencia > 7000 && experiencia <=  8000) {
+        rank = "Platina";
+    }
+    else if (experiencia > 8000 && experiencia <= 9000) {
+        rank = "Ascendente";
+    }
+    else if (experiencia > 9000 && experiencia <= 10000) {
+        rank = "Imortal";
+    }
+    else if (experiencia > 10.000) {
+        rank = "Radiante";
+    }
+
+    break;
+}
+
+console.log("O Herói de nome " +(nome) + " está no nível de " +(rank))
+
+
+
